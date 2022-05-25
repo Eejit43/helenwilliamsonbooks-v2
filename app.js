c3c910e4d97b7e3a939224547a617da9a0bf8456
Eejit <76887639+Eejit43@users.noreply.github.com>
2022-05-25 09:54:08 -0400

    switched to run on fastify

--- app.js
+++ app.js
@@ -1,77 +1,50 @@
 /* eslint-env node */
 
-import debug from 'debug';
+import fastifyStatic from '@fastify/static';
 import 'dotenv/config';
-import express from 'express';
-import expressLayouts from 'express-ejs-layouts';
-import http from 'http';
-import createError from 'http-errors';
+import ejs from 'ejs';
+import Fastify from 'fastify';
 import path from 'path';
+import pointOfView from 'point-of-view';
 import { books } from './data.js';
 
-const log = debug('helenwilliamsonbooks:server');
+// Load layouts and static assets
+const fastify = Fastify();
 
-const app = express();
-const server = http.createServer(app);
-
-const port = process.env.PORT || 3000;
+fastify.register(pointOfView, { engine: { ejs }, root: 'views', layout: '/layouts/layout.ejs' });
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
-app.set('port', port);
+fastify.register(fastifyStatic, { root: path.join(__dirname, 'public') });
 
-server.listen(port);
+// Register pages
+fastify.get('/', (req, reply) => {
+    reply.view('/index.ejs', { title: 'Home', script: '', additionalScripts: [] });
+});
 
-server.on('error', (error) => {
-    if (error.syscall !== 'listen') throw error;
+fastify.get('/books', (req, reply) => {
+    reply.view('/pages/books.ejs', { title: 'Books', script: '', additionalScripts: [], books });
+});
 
-    switch (error.code) {
-        case 'EACCES':
-            console.error(`Port ${port} requires elevated privileges`);
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(`Port ${port} is already in use`);
-            process.exit(1);
-            break;
-        default:
-            throw error;
+fastify.get('/contact', (req, reply) => {
+    reply.view('/pages/contact.ejs', { title: 'Info & Contact', script: 'contact-form', additionalScripts: [{ src: 'https://www.google.com/recaptcha/api.js', properties: 'async defer' }] });
+});
+
+// Setup error handlers
+fastify.setErrorHandler((error, request, reply) => {
+    console.log(error);
+    reply.status(error.statusCode || 500).view('/error.ejs', { title: error.message.length > 50 ? 'Internal Server Error' : error.message, message: error.message, error, script: '', additionalScripts: [] });
+});
+
+fastify.setNotFoundHandler((request, reply) => {
+    reply.status(404).view('/error.ejs', { title: 'Not Found', message: 'Not Found', error: '', script: '', additionalScripts: [] });
+});
+
+// Start server
+fastify.listen({ port: 3000 }, (err) => {
+    if (err) {
+        fastify.log.error(err);
+        process.exit(1);
     }
-});
-
-server.on('listening', () => {
-    log(`Listening on ${server.address().port}`);
-});
-
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.set('json spaces', 2);
-
-app.set('layout', 'layouts/layout');
-app.use(expressLayouts);
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', (req, res) => {
-    res.render('index', { title: 'Home', script: '', additionalScripts: [] });
-});
-
-app.get('/books', (req, res) => {
-    res.render('pages/books', { title: 'Books', script: '', additionalScripts: [], books });
-});
-
-app.get('/contact', (req, res) => {
-    res.render('pages/contact', { title: 'Info & Contact', script: 'contact-form', additionalScripts: [{ src: 'https://www.google.com/recaptcha/api.js', properties: 'async defer' }] });
-});
-
-app.use((req, res, next) => {
-    next(createError(404));
-});
-
-// eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => {
-    if (!/NotFoundError: Not Found/.test(err)) console.log(err);
-
-    res.status(err.status || 500);
-    res.render('error', { title: err.message.length > 50 ? 'Error' : err.message, message: err.message, error: err, script: '', additionalScripts: [] });
+    console.log('Server is now listening on http://localhost:3000');
 });
