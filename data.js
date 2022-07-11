5164d2f88c47b1de5478d36b12fac32784da8cad
Eejit <76887639+Eejit43@users.noreply.github.com>
2022-07-11 19:57:19 -0400

    code cleanup, convert `jpg`s to `png`s

--- data.js
+++ data.js
@@ -1,13 +1,11 @@
-export const books = [
-    {
-        name: 'Tales from Balladhoon',
-        id: 'tales-from-balladhoon-2',
-        image: 'tales-from-balladhoon-2.jpg',
+export const books = {
+    'tales-from-balladhoon-2': {
+        title: 'Tales from Balladhoon',
         description: 'The <a href="#tales-from-balladhoon" class="link"><em>original Tales from Balladhoon</em></a>, reimagined, with more original stories!<hr />Join Johnny McGlory and his friends on a delightful journey through the Irish countryside, where fairies and leprechauns are as real as the donkey that walks under the sea and the magician who summons butterflies.<br /><br />These original Irish tales by Irish native Helen L. Williamson, are a delightful glimpse into the world of imagination and an introduction to the time honored craft of Irish storytelling. These books are sure to capture the hearts of both young and old.',
-        paperback: true,
-        paperbackPrice: 14.99,
-        hardcover: true,
-        hardcoverPrice: 23.99,
+        prices: {
+            paperback: 14.99,
+            hardcover: 23.99,
+        },
         pages: 92,
         width: 8,
         height: 10,
@@ -18,22 +16,19 @@ export const books = [
             13: '9781648959523',
         },
         urls: {
-            'belle-isle': '',
             bookshop: 'https://bookshop.org/books/tales-from-balladhoon/9781648959509',
             'books-a-million': 'https://www.booksamillion.com/p/Helen-L-Williamson/9781648959523',
             'barnes-and-noble': 'https://www.barnesandnoble.com/w/tales-from-balladhoon-helen-l-williamson/1016491386',
             amazon: 'https://www.amazon.com/dp/1648959520',
         },
     },
-    {
-        name: 'Adventures in Dinglewood',
-        id: 'adventures-in-dinglewood',
-        image: 'adventures-in-dinglewood.jpg',
+    'adventures-in-dinglewood': {
+        title: 'Adventures in Dinglewood',
         description: "If you are in a hurry, you might pass by Dinglewood without paying much attention. But if you listen carefully, you just might hear the pitter-patter of small feet and the chattering of tiny voices. Turn the page—There's a world of adventure here in Dinglewood for you to discover!",
-        paperback: true,
-        paperbackPrice: 17.95,
-        hardcover: true,
-        hardcoverPrice: 26.95,
+        prices: {
+            paperback: 17.95,
+            hardcover: 26.95,
+        },
         pages: 80,
         width: 8.5,
         height: 8.5,
@@ -51,14 +46,12 @@ export const books = [
             amazon: 'https://www.amazon.com/dp/1947860216',
         },
     },
-    {
-        name: 'I Just Met a Dinosaur!',
-        id: 'i-just-met-a-dinosaur',
-        image: 'i-just-met-a-dinosaur.png',
+    'i-just-met-a-dinosaur': {
+        title: 'I Just Met a Dinosaur!',
         description: "If you met a dinosaur, what would you do? Play hide-and-seek or go to the zoo? Who knows if their skin was pink, purple, or blue? Or if they squawked, roared, or mooed? Did they polish their nails and feathers and scales, and sharpen their teeth with the ends of their tails? When you open this book of dinosaur rhymes, you'll meet some of those creatures from long-ago times!",
-        paperback: false,
-        hardcover: true,
-        hardcoverPrice: 18.95,
+        prices: {
+            hardcover: 18.95,
+        },
         pages: 32,
         width: 11.25,
         height: 8.75,
@@ -70,19 +63,16 @@ export const books = [
         },
         urls: {
             'belle-isle': 'http://www.belleislebooks.com/store/p93/I_Just_Met_a_Dinosaur!.html',
-            bookshop: '',
             'barnes-and-noble': 'https://www.barnesandnoble.com/w/i-just-met-a-dinosaur-helen-l-williamson/1124812119',
             amazon: 'https://www.amazon.com/dp/1939930677',
         },
     },
-    {
-        name: 'Higgledy-Piggledy Thoughts',
-        id: 'higgledy-piggledy-thoughts',
-        image: 'higgledy-piggledy-thoughts.jpg',
+    'higgledy-piggledy-thoughts': {
+        title: 'Higgledy-Piggledy Thoughts',
         description: 'Have you thought of a goat . . .<br />. . . in a fancy coat?<br />Or a hen that could only <em>SNORE</em>?<br /><br />Children will enjoy and be inspired by many short poems and beautiful illustrations.',
-        paperback: true,
-        paperbackPrice: 12.95,
-        hardcover: false,
+        prices: {
+            paperback: 12.95,
+        },
         pages: 48,
         width: 8.5,
         height: 11,
@@ -93,20 +83,17 @@ export const books = [
             13: '9780981925233',
         },
         urls: {
-            'belle-isle': '',
-            bookshop: '',
-            'barnes-and-noble': '',
             amazon: 'https://www.amazon.com/dp/0981925235',
         },
     },
-    {
-        name: 'A Pineapple Dream and Other Nonsense',
+    'a-pineapple-dream-and-other-nonsense': {
+        title: 'A Pineapple Dream and Other Nonsense',
         id: 'a-pineapple-dream-and-other-nonsense',
-        image: 'a-pineapple-dream-and-other-nonsense.jpg',
+        image: 'a-pineapple-dream-and-other-nonsense.png',
         description: "A Pineapple Dream and Other Nonsense is a beautifully illustrated full-color book of poems. They will delight your child and encourage their imagination as they read or listen to the poems and see the detailed drawings.<br /><br />It's Nonsense, it's Fun,<br />It tickles you tongue.<br />There's a mouse that knits<br />And a cat that sits.<br />There's a whale you'll adore<br />And a whole lot more.<br /><br />So, my friends, find a cozy nook<br />And plunge headlong into this book.",
-        paperback: true,
-        paperbackPrice: 15.95,
-        hardcover: false,
+        prices: {
+            paperback: 15.95,
+        },
         pages: 40,
         width: 8.5,
         height: 11,
@@ -117,20 +104,15 @@ export const books = [
             13: '9780981925226',
         },
         urls: {
-            'belle-isle': '',
-            bookshop: '',
-            'barnes-and-noble': '',
             amazon: 'https://www.amazon.com/dp/0981925227',
         },
     },
-    {
-        name: 'Tales from Balladhoon',
-        id: 'tales-from-balladhoon',
-        image: 'tales-from-balladhoon.jpg',
+    'tales-from-balladhoon': {
+        title: 'Tales from Balladhoon',
         description: 'Join Johnny McGlory and his friends on a delightful journey through the Irish countryside, where fairies and leprechauns are as real as the donkey that walks under the sea and the magician who summons butterflies.<br /><br />These original Irish tales by Irish native Helen L. Williamson, are a delightful glimpse into the world of imagination and an introduction to the time honored craft of Irish storytelling. These books are sure to capture the hearts of both young and old.',
-        paperback: true,
-        paperbackPrice: 12.95,
-        hardcover: false,
+        prices: {
+            paperback: 12.95,
+        },
         pages: 87,
         width: 6,
         height: 9,
@@ -141,10 +123,7 @@ export const books = [
             13: '9780981925202',
         },
         urls: {
-            'belle-isle': '',
-            bookshop: '',
-            'barnes-and-noble': '',
             amazon: 'https://www.amazon.com/dp/0981925200',
         },
     },
-];
+};
