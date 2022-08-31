4b0f9dd8287e58e77d84b983ebdcabfc95ee9b81
Eejit <76887639+Eejit43@users.noreply.github.com>
2022-08-31 18:26:17 -0400

    slight rework to `image-popup` script

--- public/scripts/image-popup.js
+++ public/scripts/image-popup.js
@@ -1,21 +1,17 @@
-// Adds modal functionality to all images on the current page with the "popup-image" class
+// Adds modal functionality to all images with the "popup-image" class
 const modal = document.getElementById('modal');
-const images = document.querySelectorAll('.popup-image');
+const images = document.querySelectorAll('img.popup-image');
 const modalImage = document.getElementById('modal-image');
 const modalCaption = document.getElementById('modal-caption');
 const closeButton = document.getElementById('close-modal');
 
-for (let i = 0; i < images.length; i++) {
-    const image = images[i];
+for (const image of images)
     image.addEventListener('click', () => {
         modal.style.display = 'block';
         modalImage.src = image.src;
         modalCaption.innerHTML = image.alt;
     });
-}
 
 [closeButton, modal].forEach((element) => {
-    element.addEventListener('click', () => {
-        modal.style.display = 'none';
-    });
+    element.addEventListener('click', () => (modal.style.display = 'none'));
 });
